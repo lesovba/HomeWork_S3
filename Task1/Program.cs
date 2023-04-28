a97cb535d8b50fb7dd1fcc04eca9a5c03368aae9
Artem Golubev <lesovba@gmail.com>
2023-04-28 15:48:20 +0500

    Completed Task#1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+int fiveDigigtNumber = Prompt("Введите пятизначное число: ");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void CheckPalindrome(int num)
+{
+    string fiveDigit = Convert.ToString(num);
+
+    if (fiveDigit.Length == 5)
+    {
+        if (fiveDigit[0] == fiveDigit[4] && fiveDigit[1] == fiveDigit[3])
+        { Console.WriteLine($"Число {num} является палиндромом"); }
+
+        else
+        { Console.WriteLine($"Число {num} НЕ является палиндромом"); }
+    }
+    else
+        Console.WriteLine("Вы ввели не пятизначное число");
+}
+
+CheckPalindrome(fiveDigigtNumber);
