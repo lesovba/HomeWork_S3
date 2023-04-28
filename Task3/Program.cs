a7cfa2a07f3461a54db89a747d138fefafc29a45
Artem Golubev <lesovba@gmail.com>
2023-04-28 17:18:06 +0500

    Completed Task#3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int n = Math.Abs(Prompt("Введите число N: "));
+int currentNum = 1;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void cubicTable(int num)
+{
+    while (currentNum <= num)
+    {
+        Console.WriteLine(Math.Pow(currentNum, 3));
+        currentNum++;
+    }
+}
+
+cubicTable(n);
+Console.ReadLine();
